3b1066467f6d92e70ba85dc33f2879926cc2f3c1
Natalia Souza <nataliasouza.ti@hotmail.com>
2020-04-21 20:10:02 -0300

    inclusão outro arquivo e info

---
+++ exercicios-js/fundamentos/variaveisEconstantes.js
@@ -0,0 +1,16 @@
+var a = 3  //declaração de variáveis com var
+let b = 4 //declaração de variáveis com let
+
+var a = 30
+
+console.log(a,b)
+
+a=300
+b=400
+
+console.log(a,b)
+
+const c = 5 //declaração de constante (const)
+
+console.log(c)
+
