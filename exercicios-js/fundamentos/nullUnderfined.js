f32343ec5c8ea7930a2f4d0945523fdf3a1a1d4d
Natalia Souza <nataliasouza.ti@hotmail.com>
2020-04-27 17:41:45 -0300

    Aula 25 - null Underfined

---
+++ exercicios-js/fundamentos/nullUnderfined.js
@@ -0,0 +1,20 @@
+let valor // não inicializada
+console.log(valor)
+
+valor = null // ausência de valor
+console.log(valor)
+
+const produto = {}
+console.log(produto.preco)
+console.log(produto)
+
+produto.preco = 3.50
+console.log(produto)
+
+produto.preco = undefined // evite atribuir underfined
+console.log(!!produto.preco)
+//delete produto.preco
+console.log(produto)
+
+produto.preco = null // sem preço
+ 
